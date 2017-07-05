3070a096d432516c0251f35ac1f0e941e3a4bfe3
Edmond <edmondcheung604@hotmail.com>
2017-07-05 03:32:13 +0000

    Added random string generator

--- express_server.js
+++ express_server.js
@@ -6,6 +6,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  var random = "";
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (var i=0; i < 6; i++) {
+    random += charset.charAt(Math.floor(Math.random() * 6));
+    }
+  return random;
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
