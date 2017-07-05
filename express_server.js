68ee7348375389171340cb3078a796058965aebe
Edmond <edmondcheung604@hotmail.com>
2017-07-05 02:54:59 +0000

    Added URL submission form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -18,9 +20,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars );
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, address: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (req, res) => {
